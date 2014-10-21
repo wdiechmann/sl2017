ff75b495d5f8028e910ee056d86876993809c76c
Walther H Diechmann <walther@diechmann.net>
2014-10-21 17:24:52 +0200

    tilføjede devise view:install så vi kan tilpasse confirmation mailer bla

--- app/models/entrance.rb
+++ app/models/entrance.rb
@@ -1,3 +1,5 @@
 class Entrance < ActiveRecord::Base
   belongs_to :employee
+  
+  validates :employee, presence: true
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,21 +27,36 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: Rails.application.secrets.domain_name,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
-  }
-  # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   domain: Rails.application.secrets.domain_name,
+  #   authentication: "plain",
+  #   enable_starttls_auto: true,
+  #   user_name: Rails.application.secrets.email_provider_username,
+  #   password: Rails.application.secrets.email_provider_password
+  # }
+  config.action_mailer.default_options = {from: 'oxen@alco.dk'}
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  # Send email in development mode?
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address:              "pbox.dk",
+    port:                 25,
+    domain:               'alco.dk',
+    user_name:            'oxen@alco.dk',
+    password:             'ox!6562',
+    authentication:       'login',
+    enable_starttls_auto: false  
+  }
+
+  # # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.raise_delivery_errors = true
+  # # Send email in development mode?
+  # config.action_mailer.perform_deliveries = true
 
 
   # Adds additional error checking when serving assets at runtime.
