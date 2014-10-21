bb2356a2e7f314d7b7a17f1d72192b25035de92e
Walther H Diechmann <walther@diechmann.net>
2014-10-21 17:19:41 +0200

    added pundit roles to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # authorize models
+  include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+   private
+
+   def user_not_authorized
+     flash[:error] = "You are not authorized to perform this action."
+     redirect_to(request.referrer || root_path)
+   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,15 @@ class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
+  validates :name, presence: true
+
   def set_default_role
     self.role ||= :user
+  end
+  
+  def good_roles
+
+    User.roles.keys.map {|r| ([r.titleize,r] if User.roles[role] >= User.roles[r]) }
   end
 
   # Include default devise modules. Others available are:
