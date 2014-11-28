b1760c3fd320a3450268d83d3d3a90cd1a30e679
Walther H Diechmann <walther@diechmann.net>
2014-11-28 13:40:29 +0100

    rettet flere småfejl - bla i emails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) ||
+      if resource.is_a?(User) #&& resource.can_publish?
+        jobs_url
+      else
+        super
+      end
+  end  
+
   protected
 
     def configure_permitted_parameters
