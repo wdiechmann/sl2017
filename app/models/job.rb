b117a212ea33027fe97d3c29b9809a0b03def045
Walther H Diechmann <walther@diechmann.net>
2014-11-28 14:55:39 +0100

    tilføjet oversigt over jobs - samt lidt flere smårettelser

--- app/models/job.rb
+++ app/models/job.rb
@@ -7,4 +7,5 @@ class Job < ActiveRecord::Base
       message: "%{value} is not a valid priority" }
 		validates :description,  presence: true
 
+    scope :vacancies, -> { where( "vacancies > 0 and delegated_at is null").limit(20) }
 end
--- app/policies/jobber_policy.rb
+++ app/policies/jobber_policy.rb
@@ -10,5 +10,8 @@ class JobberPolicy < ApplicationPolicy
     true
   end
 
+  def confirmation?
+    true
+  end
 
 end
