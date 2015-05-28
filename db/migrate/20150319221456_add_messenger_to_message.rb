36e251c2aa650aadce55393b831b8118d68a7d2e
Walther H Diechmann <walther@diechmann.net>
2015-05-28 22:30:54 +0200

    lokale rettelser - hmmm - gad vist om de også klarer sig op til repo?

--- db/migrate/20150319221456_add_messenger_to_message.rb
+++ db/migrate/20150319221456_add_messenger_to_message.rb
@@ -2,5 +2,7 @@ class AddMessengerToMessage < ActiveRecord::Migration
   def change
     add_column :messages, :messenger_id, :integer
     add_column :messages, :messenger_type, :string
+
+    execute "ALTER TABLE messages CHANGE body body text CHARACTER SET 'utf8mb4' COLLATE utf8mb4_unicode_ci;"
   end
 end
